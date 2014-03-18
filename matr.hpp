75b8a1e9ec7f492480a165156f00f88b60790aad
Daniel Escoz <darkhogg@gmail.com>
2014-03-18 09:40:54 +0100

    Added some matrix methods

--- matr.hpp
+++ matr.hpp
@@ -5,9 +5,66 @@ namespace igr {
 
   template <typename C=double>
   class matr {
+    private:
+      C data[4][4];
 
+    public:
+      matr () : data{{
+        {0.0, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 1.0}
+      }}
+      {}
+
+      const C& operator() (std::size_t i, std::size_t j) const;
+      C& operator() (std::size_t i, std::size_t j);
+
+      template<typename VC>
+      static matr<VC> make_translation (vec<VC> translation);
+
+      template<typename VC>
+      static matr<VC> make_scalation (vec<VC> scalation);
+
+      template<typename VC>
+      static matr<VC> make_rotation (vec<VC> around, double angle);
   };
 
+
+  /* ======================= */
+  /* === POSITION ACCESS === */
+
+  template<typename C>
+  const C& matr<C>::operator() (std::size_t i, std::size_t j) const {
+    return data[i][j];
+  }
+
+
+  template<typename C>
+  C& matr<C>::operator() (std::size_t i, std::size_t j) {
+    return data[i][j];
+  }
+
+
+  /* =========================== */
+  /* === STATIC CONSTRUCTION === */
+
+    template<typename VC>
+    matr<VC> matr::make_translation (vec<VC> translation) {
+      matr<VC> m;
+
+      return m;
+    }
+
+    template<typename VC>
+    matr<VC> matr::make_scalation (vec<VC> scalation) {
+
+    }
+
+    template<typename VC>
+    matr<VC> matr::make_rotation (vec<VC> around, double angle) {
+
+    }
 
 }
 
